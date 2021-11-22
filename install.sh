83712ab3c62effc7f1f3e4239b01f9850eb45f1c
Turge08 <Turge08@gmail.com>
2021-11-22 17:34:38 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+KLIPPER_PATH="${HOME}/klipper_config"
+SYSTEMDDIR="/etc/systemd/system"
+
+# Step 1:  Verify Klipper has been installed
+check_klipper()
+{
+    if [ "$(sudo systemctl list-units --full -all -t service --no-legend | grep -F "klipper.service")" ]; then
+        echo "Klipper service found!"
+    else
+        echo "Klipper service not found, please install Klipper first"
+        exit -1
+    fi
+
+}
+
+# Step 2: link extension to Klipper
+link_extension()
+{
+    target=$(find ~/klipper_config -name "print_area_bed_mesh.cfg")
+    if [[ -z "$target" ]]
+    then
+	target="$HOME/klipper_config/print_area_bed_mesh.cfg"
+    fi
+    echo "Linking ${SRCDIR}/print_area_bed_mesh.cfg to $target"
+    ln -sf "${SRCDIR}/print_area_bed_mesh.cfg" "$target"
+}
+
+# Step 3: restarting Klipper
+restart_klipper()
+{
+    echo "Restarting Klipper..."
+    sudo systemctl restart klipper
+}
+
+# Helper functions
+verify_ready()
+{
+    if [ "$EUID" -eq 0 ]; then
+        echo "This script must not run as root"
+        exit -1
+    fi
+}
+
+# Force script to exit if an error occurs
+set -e
+
+# Find SRCDIR from the pathname of this script
+SRCDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/ && pwd )"
+
+# Parse command line arguments
+while getopts "k:" arg; do
+    case $arg in
+        k) KLIPPER_PATH=$OPTARG;;
+    esac
+done
+
+# Run steps
+verify_ready
+link_extension
+restart_klipper
